406e86cf69d44d58bd13828ba348876fba39ddf4
Harish.K <kollatiharish@gmail.com>
2025-04-11 00:41:56 +0530

    NO TICKET:firstcut of item label container

--- src/Components/ReUsable/TitledDataTable.tsx
+++ src/Components/ReUsable/TitledDataTable.tsx
@@ -1,16 +1,16 @@
 import React from "react";
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Grid } from "@mui/material";
 import InfoOutline from "@mui/icons-material/InfoOutlined";
 import DataTable from "./MasterDataTable"; // Adjust the import path as needed
 
 interface Column {
-    id: string;
-    label: string;
-    isDropdown?: boolean;
-    options?: string[];
-    edit?: boolean;
-    editSelect?: boolean;
-  }
+  id: string;
+  label: string;
+  isDropdown?: boolean;
+  options?: string[];
+  edit?: boolean;
+  editSelect?: boolean;
+}
 
 interface TitledDataTableProps<T> {
   title?: string;
@@ -21,7 +21,7 @@ interface TitledDataTableProps<T> {
   borderRadius?: string | number;
   titleColor?: string;
   infoIconColor?: string;
-  firstRow?:boolean
+  firstRow?: boolean
 }
 
 const TitledDataTable = <T extends Record<string, any>>({
@@ -33,8 +33,8 @@ const TitledDataTable = <T extends Record<string, any>>({
   borderRadius = "16px",
   titleColor = "#2F2FF",
   infoIconColor = "#9F9F9F",
-  firstRow=false
-}:TitledDataTableProps<T>) => {
+  firstRow = false
+}: TitledDataTableProps<T>) => {
   return (
     <Box
       sx={{
@@ -66,13 +66,79 @@ const TitledDataTable = <T extends Record<string, any>>({
           sx={{ color: infoIconColor, width: 20, height: 20 }}
         />
       </Box>
-      <DataTable
-        columns={columns}
-        data={data}
-        setData={setData}
-        tableTitle={true}
-        firstRow={firstRow ?true:false}
-      />
+      <Box sx={{
+        borderBottom: `1px solid ${borderColor}`,
+        borderRadius: borderRadius,
+      }}>
+        <DataTable
+          columns={columns}
+          data={data}
+          setData={setData}
+          tableTitle={true}
+          firstRow={firstRow ? true : false}
+        />
+      </Box>
+      <Box className="mt-4 p-4 ">
+        <Box>
+          <Grid container spacing={2} pt={1}>
+            <Grid size={{ xs: 12, sm: 6, md: 4, lg: 3 }}>
+              <Typography variant="body2" color="text.secondary" fontWeight={500}>
+                Unit Effectivity Number
+              </Typography>
+              <Typography
+                variant="body1"
+                sx={{
+                  mt: 0.5,
+                  wordBreak: "break-word",
+                  whiteSpace: "pre-line",
+                }}
+              >
+                UEN-20240801
+              </Typography>
+            </Grid>
+            <Grid size={{ xs: 12, sm: 6, md: 4, lg: 3 }}>
+              {/* <Box sx={{ mt: 2 }}> */}
+              <Typography
+                variant="body2"
+                color="text.secondary"
+                fontWeight={500}
+              >
+                ITEM Code
+              </Typography>
+              <Typography
+                variant="body1"
+                sx={{
+                  mt: 0.5,
+                  wordBreak: "break-word",
+                  whiteSpace: "pre-line",
+                }}
+              >
+                KK-50G-123
+              </Typography>
+            </Grid>
+            <Grid size={{ xs: 12, sm: 6, md: 4, lg: 3 }}>
+              <Typography
+                variant="body2"
+                color="text.secondary"
+                fontWeight={500}
+              >
+                Jar/Cap
+              </Typography>
+              <Typography
+                variant="body1"
+                sx={{
+                  mt: 0.5,
+                  wordBreak: "break-word",
+                  whiteSpace: "pre-line",
+                }}
+              >
+                N/A (For flexible packaging)
+              </Typography>
+              {/* </Box> */}
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
     </Box>
   );
 };
