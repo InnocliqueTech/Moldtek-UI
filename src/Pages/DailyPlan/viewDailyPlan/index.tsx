b0680228771de1709ebed519975c25f2afe0cfc3
lavanyaYarlagadda <lavanyaYarlagadda>
2025-04-11 11:13:38 +0530

    Fixed the build errors

--- src/Pages/DailyPlan/viewDailyPlan/index.tsx
+++ src/Pages/DailyPlan/viewDailyPlan/index.tsx
@@ -1,32 +1,17 @@
 import React from "react";
 import { Box } from "@mui/material";
-import OrderCard from "../../../Components/ReUsable/OrderCard";
 import TabsComponent from "../../../Components/ReUsable/Tabs";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../../../store";
 import { setSelectedTab } from "../../../store/slices/viewMasterDataSlice";
 import MakeReady from "./makeReady";
-import customerImage from "../../../assets/Images/customerPicture.png";
-import { useNavigate } from "react-router-dom";
 import PrintingReport from "./PrintingReport";
 import LaminationReport from "./LaminationReport";
 import LabelCutting from "./LabelCutting";
 import TravelCard from "./TravelCard";
 import CommenCard from "./commonCard";
 
-const mockData = [
-  { label: "Unit Effectivity Number", value: "UEN-20240801" },
-  { label: "Customer Name", value: "Nestlé" },
-  { label: "Customer Picture", value: customerImage, isImage: true },
-  { label: "ITEM Code", value: "KK-50G-123" },
-  {
-    label: "Brand Name & Pack Description",
-    value:
-      "0 LTR_AP_DTS_L.WT <APEX ULTIMA PROTEK TOPCOAT> [CODE:P34779J] (IML) ASIAN PAINTS",
-  },
-  { label: "Jar/Cap", value: "N/A (For flexible packaging)" },
-  { label: "Structure", value: "PET" },
-];
+
 
 const tabs = [
   "Make Ready",
@@ -41,14 +26,10 @@ const ViewDailyPlan: React.FC = () => {
   const { selectedTab } = useSelector(
     (state: RootState) => state.viewMasterData
   );
-  const navigate = useNavigate();
   const handleTabChange = (_event: React.SyntheticEvent, newValue: number) => {
     dispatch(setSelectedTab(newValue));
   };
 
-  const handleBack = () => {
-    navigate("/masterData");
-  };
 
   return (
     <Box
